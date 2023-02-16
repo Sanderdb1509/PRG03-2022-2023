f988541d8916d3eedf741f6f3438663258684123
KokoDoko <benni_jolink@hotmail.com>
2023-02-16 14:13:50 +0100

    simplify array sort

--- week3/goudkistje-start/js/main.js
+++ week3/goudkistje-start/js/main.js
@@ -50,20 +50,11 @@ function writeFeedbackMessage(text)
 }
 
 /**
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- * @link http://stackoverflow.com/a/12646864
- *
+ * Randomize array using sort
  * @param array
  * @returns {*}
  */
 function shuffleArray(array)
 {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
+    return arr.sort(() => (Math.random() - 0.5))
 }
