cebdb512fe4f622884188663874134d64864e0d3
antwanvdm <antwanvdm@gmail.com>
2023-03-14 15:35:47 +0100

    Updated code web api's

--- week5/web-apis/geolocation/js/main.js
+++ week5/web-apis/geolocation/js/main.js
@@ -1,25 +1,28 @@
 window.addEventListener('load', init);
-let $target = null;
-let $button = null;
+let target = null;
+let button = null;
 
 function init()
 {
-    $target = document.getElementById('target');
-    $button = document.getElementById('button');
+  target = document.getElementById('target');
+  button = document.getElementById('button');
 
-    if (typeof navigator.geolocation === "undefined") {
-        $target.innerText = 'Geolocation API not supported.';
-        return;
-    }
+  if (typeof navigator.geolocation === 'undefined') {
+    target.innerText = 'Geolocation API not supported.';
+    return;
+  }
 
-    $button.addEventListener('click', function () {
-        navigator.geolocation.getCurrentPosition(showCurrentLocation);
-        navigator.geolocation.watchPosition(showCurrentLocation);
-    });
+  button.addEventListener('click', buttonClickHandler);
+}
+
+function buttonClickHandler()
+{
+  navigator.geolocation.getCurrentPosition(showCurrentLocation);
+  navigator.geolocation.watchPosition(showCurrentLocation);
 }
 
 function showCurrentLocation(location)
 {
-    $button.remove();
-    $target.innerHTML = (location.coords.latitude + ', ' + location.coords.longitude);
+  button.remove();
+  target.innerHTML = (location.coords.latitude + ', ' + location.coords.longitude);
 }
